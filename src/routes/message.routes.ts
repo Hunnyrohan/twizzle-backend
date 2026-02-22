5a422bf5f5faeb49506e8a76ee9298f2c3737f29
Rohan <yadavroohan4545@gmail.com>
2026-02-22 20:21:23 +0545

    Message done

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -2,19 +2,7 @@ import { Router } from 'express';
 import * as messageController from '../controllers/message.controller';
 import * as messageValidator from '../validators/message.validator';
 import authMiddleware from '../middlewares/auth.middleware';
-import { validate } from '../middlewares/validation.middleware'; // Assuming this exists or I'm using a generic one, but user listed 'zod validation' in requirements. 
-// I'll assume standard middleware pattern or define a simple one if missing.
-// Checking file list, I saw 'validators' folder but not validation middleware in the list explicitly, but 'middlewares' folder has 'input-validation.middleware.ts' possibly? 
-// Let's check 'middlewares' folder content again from previous step.
-// list_dir of middlewares showed: admin.middleware.ts, auth.middleware.ts, error.middleware.ts, protect.ts
-// I don't see a validation middleware. I will create a simple inline one or import 'zod' and use it manually in controller if needed, 
-// but standard practice is middleware. I will create a `validateRequest` adapter here or reuse one if I find it.
-// For now, I'll inline the Zod validation in the routes or create a utility.
-
-// Actually, I'll assume I can just use the schemas in the controller or a middleware helper. 
-// Since I don't want to create extra files not requested if possible, I'll add a simple validation helper in this file or a separate one if strict.
-// User asked for "zod validation for params/query/body".
-
+import { upload } from '../config/upload';
 import { z, AnyZodObject } from 'zod';
 
 const validateRequest = (schema: AnyZodObject) => (req: any, res: any, next: any) => {
@@ -46,6 +34,7 @@ router.get(
 
 router.post(
     '/conversations/:id/messages',
+    upload.array('images', 5),
     validateRequest(messageValidator.sendMessageSchema),
     messageController.sendMessage
 );
