691736c81d8f58f792e712693a8ab068a5e5ebcd
Rohan <yadavroohan4545@gmail.com>
2026-02-22 20:20:32 +0545

    Authroute

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { login, register } from '../controllers/auth.controller';
+import { login, register, forgotPassword, resetPassword, googleLogin } from '../controllers/auth.controller';
 const router = Router();
 
 router.post('/login', login);
 router.post('/register', register);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
+router.post('/google-login', googleLogin);
 
 export default router;
