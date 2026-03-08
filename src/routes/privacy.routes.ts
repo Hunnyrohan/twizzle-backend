3135e2680c30f69e04c41b90587378372ec85e01
Rohan <yadavroohan4545@gmail.com>
2026-03-08 23:13:12 +0545

    chore: update src/routes/privacy.routes.ts

--- src/routes/privacy.routes.ts
+++ src/routes/privacy.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import authMiddleware from '../middlewares/auth.middleware';
-import { updatePrivacy } from '../controllers/privacy.controller';
+import { updatePrivacy, getPrivacy } from '../controllers/privacy.controller';
 
 const router = Router();
 
+router.get('/me', authMiddleware, getPrivacy);
 router.patch('/me', authMiddleware, updatePrivacy);
 
 export default router;
